02167796ed214264948817cf8dfcd650d0d5f6ec
ellucianmoodle <ILPDev@ellucian.com>
2014-12-09 13:39:38 -0500

    Test commit
    Test commit into 4.2 branch.

--- version.php
+++ version.php
@@ -1,4 +1,19 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 /**
  * ILP Integration
  *
@@ -29,7 +44,7 @@
  *
  **/
 
-$plugin->version = 2013030500;
+$plugin->version = 2014120201;
 $plugin->requires = 2013111803;
 $plugin->component = 'block_intelligent_learning';
 $plugin->release   = '2.6.0 (Build: 20140603)';
